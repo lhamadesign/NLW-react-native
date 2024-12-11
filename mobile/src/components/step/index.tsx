3848b74a99c621b93bd3305588bb37995c425b02
lhama <contato@lhama.design>
2024-12-11 16:31:00 -0300

    new components and API integration

--- mobile/src/components/step/index.tsx
+++ mobile/src/components/step/index.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from "react-native";
 import { s } from "./style";
 import { IconProps } from "@tabler/icons-react-native";
-// import { colors } from "@/styles/theme";
+import { colors } from "@/styles/theme";
 
 export type Props = {
     title: string,
@@ -12,7 +12,7 @@ export type Props = {
 export function Step({title, description, icon: Icon}: Props) {
     return (
         <View style={s.container}>
-            {Icon && <Icon size={32} />}
+            {Icon && <Icon size={32} color={colors.red.base} />}
             <View style={s.details}>
                 <Text style={s.title}>{title}</Text>
                 <Text style={s.description}>{description}</Text>
